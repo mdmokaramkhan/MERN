d161a496de244add54be3608c3e0d40bbbb14630
mdmokaramkhan <mohammadmokaram23@lpu.in>
2026-01-28 10:38:03 +0530

    js-fundamentals file addded

---
+++ fundamental.js
@@ -0,0 +1,85 @@
+// let city = "Banglore"
+// city = "Delhi"
+// console.log(city)
+
+// const country = "India"
+// console.log(country)
+
+// // country = "USA"
+// console.log(country)
+
+// let marks = 90
+// let student = "John" // string
+// let isPass = true // boolean
+// let isFail = false // boolean
+// let x; // undefined
+// let y = null; // null
+
+// console.log("marks: ", marks)
+// console.log("student: ", student)
+// console.log("isPass: ", isPass)
+// console.log("isFail: ", isFail)
+// console.log("x: ", x)
+// console.log("y: ", y)
+
+// // type of variables
+// console.log("type of marks: ", typeof marks)
+// console.log("type of student: ", typeof student)
+// console.log("type of isPass: ", typeof isPass)
+// console.log("type of isFail: ", typeof isFail)
+// console.log("type of x: ", typeof x)
+// console.log("type of y: ", typeof y)
+
+// // Other number types:
+// let a = NaN
+// let b = Infinity
+// let c = -Infinity
+
+// // console.log("a: ", a)
+// // console.log("b: ", b)
+// // console.log("c: ", c)
+
+// // console.log("type of a: ", typeof a)
+// // console.log("type of b: ", typeof b)
+// // console.log("type of c: ", typeof c)
+
+
+// let a = Infinity
+// console.log("a: ", a) // Infinity
+// console.log("type of a: ", typeof a)
+
+// let b = -Infinity
+// console.log("b: ", b) // -Infinity
+// console.log("type of b: ", typeof b)
+
+// let c = NaN
+// console.log("c: ", c) // NaN
+// console.log("type of c: ", typeof c)
+
+// let d = 1.23e4
+
+
+// let age = "20"
+// Number(age)
+// console.log("age: ", parseInt("20.243"))
+// console.log("age: ", age)
+// console.log("type of age: ", typeof age)
+
+// Strings 
+
+let firstName = "John"
+let course = 'JavaScript'
+
+firstName.length
+firstName.toUpperCase()
+firstName.toLowerCase()
+firstName.includes("o")
+
+// Template Literals
+
+let message = `Hello ${firstName}, welcome to ${course}`
+console.log(message)
+
+let a = 20, b = 30
+let result = `The sum of ${a} and ${b} is ${a + b}`
+console.log(result)
